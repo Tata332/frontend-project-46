a8cad3f802277429419b00455499c79ae0ed4afc
Tata <marnovaanastasia.default@gmail.com>
2025-03-25 16:39:15 +0300

    add argument support and -f option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-
-const program = new Command();
+import { program } from 'commander';
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .helpOption('-h, --help', 'display help for command');
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse();
