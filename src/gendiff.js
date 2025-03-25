385eaff61326f93bbcac02b9101ae4db6a0dceec
Tata <marnovaanastasia.default@gmail.com>
2025-03-25 17:28:25 +0300

    добавлен lodash и исправлен код

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
 const readFile = (filepath) => {
   const absolutePath = path.resolve(process.cwd(), filepath);
@@ -11,17 +12,12 @@ const genDiff = (filepath1, filepath2) => {
   const data1 = readFile(filepath1);
   const data2 = readFile(filepath2);
 
-  const keys = new Set([...Object.keys(data1), ...Object.keys(data2)]);
-  const diff = [...keys].map((key) => {
-    if (!Object.hasOwn(data2, key)) {
-      return `- ${key}: ${data1[key]}`;
-    }
-    if (!Object.hasOwn(data1, key)) {
-      return `+ ${key}: ${data2[key]}`;
-    }
-    if (data1[key] !== data2[key]) {
-      return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
-    }
+  const keys = _.sortBy([...new Set([...Object.keys(data1), ...Object.keys(data2)])]);
+
+  const diff = keys.map((key) => {
+    if (!Object.hasOwn(data2, key)) return `- ${key}: ${data1[key]}`;
+    if (!Object.hasOwn(data1, key)) return `+ ${key}: ${data2[key]}`;
+    if (data1[key] !== data2[key]) return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
     return `  ${key}: ${data1[key]}`;
   });
 
