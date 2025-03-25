fc06c224ccf9a60f2cfcfff84446dd7982194251
Tata <marnovaanastasia.default@gmail.com>
2025-03-25 17:14:20 +0300

    bug fixes

--- src/gendiff.js
+++ src/gendiff.js
@@ -10,8 +10,22 @@ const readFile = (filepath) => {
 const genDiff = (filepath1, filepath2) => {
   const data1 = readFile(filepath1);
   const data2 = readFile(filepath2);
-  
-  return JSON.stringify(data1, null, 2) + '\n' + JSON.stringify(data2, null, 2);
+
+  const keys = new Set([...Object.keys(data1), ...Object.keys(data2)]);
+  const diff = [...keys].map((key) => {
+    if (!Object.hasOwn(data2, key)) {
+      return `- ${key}: ${data1[key]}`;
+    }
+    if (!Object.hasOwn(data1, key)) {
+      return `+ ${key}: ${data2[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
+    }
+    return `  ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${diff.join('\n')}\n}`;
 };
 
 export default genDiff;
